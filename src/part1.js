b47e83dfa1bd0a35492167d275f8c3f9c5444fae
Alfredo Ficcadenti <aficcadenti@homeaway.com>
2018-10-18 22:24:14 +0100

    added part1

---
+++ src/part1.js
@@ -0,0 +1,17 @@
+for (i = 100; i < 200; i++) {
+    var rem3 = i % 3;
+    var rem4 = i % 4;
+    
+    if (rem3==rem4 && rem4==0) {
+      console.log("HomeAway");
+    }
+    else if (rem4==0) {
+      console.log("Away");
+    }
+    else if (rem3==0) {
+      console.log("Home");
+    }
+    else {
+      console.log(i);
+    }
+  }
