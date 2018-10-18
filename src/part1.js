5803b2bb22681f46e9c91d752d9f76a0f689450a
Alfredo Ficcadenti <alfredoficcadenti@gmail.com>
2018-10-18 22:53:58 +0100

    typo correction on part1

--- src/part1.js
+++ src/part1.js
@@ -1,8 +1,8 @@
-for (i = 100; i < 200; i++) {
+for (i = 100; i <= 200; i++) {
     var rem3 = i % 3;
     var rem4 = i % 4;
-    
-    if (rem3==rem4 && rem4==0) {
+
+    if (rem3==0 && rem4==0) {
       console.log("HomeAway");
     }
     else if (rem4==0) {
