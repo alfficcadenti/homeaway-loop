be4165f509adf0b3b561cb871a24be5647dc7986
Alfredo Ficcadenti <alfredoficcadenti@gmail.com>
2018-10-18 22:53:03 +0100

    added part2 and typo correction on part1

---
+++ src/part2.js
@@ -0,0 +1,24 @@
+function homeawayLoop(range, multiples, words) {
+
+if(range.length!==2){return "illegal range";}
+if(multiples.length!==2){return "choose 2 numbers";}
+if(words.length!==2){return "choose 2 words";}
+
+for (i = range[0]; i <= range[1]; i++) {
+      var rem1 = i % multiples[0];
+      var rem2 = i % multiples[1];
+
+      if (rem1==0 && rem2==0) {
+        console.log(words.join(''));
+      }
+      else if (rem1==0) {
+        console.log(words[0]);
+      }
+      else if (rem2==0) {
+        console.log(words[1]);
+      }
+      else {
+        console.log(i);
+      }
+    }
+}
